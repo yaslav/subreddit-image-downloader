f8fbfa8396188789f353060a38cb93ba081752de
yaslav <slava.yavchenko@gmail.com>
2021-07-11 15:21:38 +0200

    Check and delete empty files after download

--- srid.sh
+++ srid.sh
@@ -14,6 +14,7 @@ FAKE_USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (K
 TOP_URL="https://www.reddit.com/r/$SUBREDDIT/top.json?limit=100&t=$TIME"
 
 echo "Attempting to download JSON metadata from /r/$SUBREDDIT"
+rm -f posts.json
 http_code=$(curl -A "$FAKE_USER_AGENT" -s -w "%{http_code}" "$TOP_URL" -o posts.json)
 if [ "$http_code" -eq "429" ]; then
     echo "Error 429: too many requests. Maybe try changing the user agent or wait a couple of minutes"
@@ -21,14 +22,13 @@ if [ "$http_code" -eq "429" ]; then
 elif [ "$http_code" -eq "200" ]; then
     echo "Successfully parsed metadata from $SUBREDDIT"
 fi
-echo "http_code is $http_code"
 urls=$(jq -r '.data.children | .[].data | select(.post_hint=="image") | .url' posts.json)
 
-if [ ! -d $DL_FOLDER ]; then
+if [ ! -d "$DL_FOLDER" ]; then
     echo "$DL_FOLDER does not exist, creating"
-    mkdir $DL_FOLDER
+    mkdir "$DL_FOLDER"
 fi
-dl_folder_abs=$(realpath $DL_FOLDER)
+dl_folder_abs=$(realpath "$DL_FOLDER")
 
 for url in $urls; do
     filename=$(basename "$url")
@@ -36,6 +36,10 @@ for url in $urls; do
     if [ ! -f "$filename_abs" ]; then
         echo "Downloading $filename from $url"
         curl "$url" -s -o "$filename_abs"
+        if file "$filename_abs" | grep empty > /dev/null; then
+            echo "Error downloading file $url"
+            rm "$filename_abs"
+        fi
     else
         echo "File $filename exists under $filename_abs, skipping"
     fi
