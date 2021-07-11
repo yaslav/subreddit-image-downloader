48be6a60375190c1a592fa14f35ba96ed3e285f6
yaslav <slava.yavchenko@gmail.com>
2021-07-11 18:21:42 +0200

    Refactoring and printing result of download

--- srid.sh
+++ srid.sh
@@ -74,53 +74,76 @@ process_args() {
     esac
 }
 
+debug_print() {
+    $VERBOSE && echo $1
+}
+
+check_http_code() {
+    debug_print "http_code: $http_code"
+    http_code=$1
+    if [ "$http_code" -eq "429" ]; then
+        echo "Error 429: too many requests. Maybe try changing the user agent or wait a couple of minutes"
+        exit 1
+    elif [ "$http_code" -eq "302" ]; then
+        echo "Invalid subreddit /r/$SUBREDDIT, please check the subreddit parameter"
+        exit 1
+    elif [ "$http_code" -eq "200" ]; then
+        echo "Successfully parsed metadata from /r/$SUBREDDIT"
+    fi
+}
+
 parse_args "$@"
 process_args
 
-$VERBOSE && echo "DIRECTORY: $DL_DIR, FREQUENCY: $FREQUENCY, SUBREDDIT: $SUBREDDIT, VERBOSE: $VERBOSE"
-
-FAKE_USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-
-TOP_URL="https://www.reddit.com/r/$SUBREDDIT/top.json?limit=100&t=$FREQUENCY"
-
-$VERBOSE && echo "Attempting to download JSON metadata from /r/$SUBREDDIT"
 rm -f posts.json
-http_code=$(curl -A "$FAKE_USER_AGENT" -s -w "%{http_code}" "$TOP_URL" -o posts.json)
-$VERBOSE && echo "http_code: $http_code"
-if [ "$http_code" -eq "429" ]; then
-    echo "Error 429: too many requests. Maybe try changing the user agent or wait a couple of minutes"
-    exit 1
-elif [ "$http_code" -eq "302" ]; then
-    echo "Invalid subreddit /r/$SUBREDDIT, please check the subreddit parameter"
-    exit 1
-elif [ "$http_code" -eq "200" ]; then
-    echo "Successfully parsed metadata from /r/$SUBREDDIT"
-fi
 
-length=$(jq -r '.data.children | length' posts.json)
+debug_print "DIRECTORY: $DL_DIR, FREQUENCY: $FREQUENCY, SUBREDDIT: $SUBREDDIT, VERBOSE: $VERBOSE"
+
+# needs to be passed otherwise reddit blocks requests
+fake_user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+
+top_url="https://www.reddit.com/r/$SUBREDDIT/top.json?limit=100&t=$FREQUENCY"
+
+debug_print "Attempting to download JSON metadata from /r/$SUBREDDIT"
+
+http_code=$(curl -A "$fake_user_agent" -s -w "%{http_code}" "$top_url" -o posts.json)
+check_http_code "$http_code"
+
+number_images=$(jq -r '[.data.children | .[].data | select(.post_hint=="image")] | length' posts.json)
 urls=$(jq -r '.data.children | .[].data | select(.post_hint=="image") | .url' posts.json)
 
 if [ ! -d "$DL_DIR" ]; then
-    $VERBOSE && echo "$DL_DIR does not exist, creating"
+    debug_print "$DL_DIR does not exist, creating"
     mkdir "$DL_DIR"
 fi
+
 dl_folder_abs=$(realpath "$DL_DIR")
 
-echo "Downloading top $length images of $FREQUENCY_STRING from /r/$SUBREDDIT"
+echo "Downloading top $number_images images of $FREQUENCY_STRING from /r/$SUBREDDIT"
+
+downloaded=0
+skipped=0
+errors=0
+total=0
 
 for url in $urls; do
+    total=$((total+1))
     filename=$(basename "$url")
     filename_abs="$dl_folder_abs/$filename"
     if [ ! -f "$filename_abs" ]; then
-        $VERBOSE && echo "Downloading $filename from $url"
+        debug_print "Downloading $filename from $url"
         curl "$url" -s -o "$filename_abs"
         if file "$filename_abs" | grep empty > /dev/null; then
-            $VERBOSE && echo "Error downloading file $url"
+            debug_print "Error downloading file $url"
             rm "$filename_abs"
+	    errors=$((errors+1))
+	else
+	    downloaded=$((downloaded+1))
         fi
     else
-        $VERBOSE && cho "File $filename exists under $filename_abs, skipping"
+        debug_print "File $filename exists under $filename_abs, skipping"
+        skipped=$((skipped+1))
     fi
 done
 
-echo "Done"
+echo "Finished. Total: $total,  downloaded: $downloaded, skipped: $skipped, errors: $errors"
