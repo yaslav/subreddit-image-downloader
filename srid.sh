60148fa8b82d8c5dd1a64265a0435cb5fd4a0d9b
yaslav <slava.yavchenko@gmail.com>
2021-07-11 22:21:57 +0200

    Add option to pass json file with parameters

--- srid.sh
+++ srid.sh
@@ -6,6 +6,7 @@ VERBOSE=false
 SUBREDDIT=""
 DL_DIR=""
 FREQUENCY=""
+JSON_FILE=""
 ## number of characters for progress bar in the terminal
 PROGRESS_BAR_WIDTH=90
 ## user agent for curl of reddit json api
@@ -58,11 +59,12 @@ help() {
     echo "  -d|--directory          Directory to store downloaded images"
     echo "  -f|--frequency          Post frequency (day|week|month|year|all)"
     echo "  -s|--subreddit          Subreddit name from which to download images"
+    echo "  -j|--json               Alternatively: json file containing above parameters"
 }
 
 
 parse_args() {
-    options=$(getopt -l "help,directory:,freq:,subreddit:,verbose" -o "hd:f:s:v" -- "$@")
+    options=$(getopt -l "help,directory:,freq:,json-file:,subreddit:,verbose" -o "hd:f:j:s:v" -- "$@")
     eval set -- "$options"
 
     while true
@@ -75,6 +77,10 @@ parse_args() {
     -d|--directory)
         shift
         DL_DIR=$1
+        ;;
+    -j|--json-file)
+        shift
+        JSON_FILE=$1
         ;;
     -f|--frequency)
         shift
@@ -94,7 +100,8 @@ parse_args() {
     shift
     done
 }
-process_args() {
+
+check_args() {
     if [ -z "$SUBREDDIT" ]; then
         echo "Error: subreddit name not provided"
         echo "Pass subreddit name via -s|--subreddit argument"
@@ -110,18 +117,54 @@ process_args() {
        echo "Error: Invalid frequency $FREQUENCY provided. Either leave parameter -f|--frequency empty or set to day|week|month|year|all"
        exit 1 
     fi
+
+}
+
+check_json() {
+    if [ ! -f "$JSON_FILE" ]; then
+        echo "Error: $JSON_FILE does not exist"
+        exit 1
+    fi
+
+    jq -e . "$JSON_FILE"  >/dev/null 2>&1
+    if [ $? -eq 4 ]; then
+        echo "JSON file $JSON_FILE is invalid, please check it"
+        exit 1
+    fi
+    number_entries=$(jq ". | length" "$JSON_FILE")
+    if [ "$number_entries" -eq 0 ]; then
+        echo "No entries found in $JSON_FILE"
+        exit 1
+    fi
+    for entry in $(seq 0 $((number_entries-1))); do
+        subreddit=$(jq -r ".[$entry].subreddit" "$JSON_FILE")
+        frequency=$(jq -r ".[$entry].frequency" "$JSON_FILE")
+        directory=$(jq -r ".[$entry].directory" "$JSON_FILE")
+        if [ "$subreddit" = "null" ]; then
+            echo "Missing or invalid 'subreddit' variable in entry #$entry in $JSON_FILE"
+            exit 1
+        elif [ "$frequency"  = null ]; then
+            echo "Missing or invalid 'frequency' variable in entry #$entry in $JSON_FILE"
+            exit 1
+        elif [ "$directory" = null ]; then
+            echo "Missing or invalid 'directory' variable in entry #$entry in $JSON_FILE"
+            exit 1
+        fi
+    done
+
 }
 
 get_frequency_string(){
     frequency=$1
     frequency_string=""
-    case "$FREQUENCY" in
+    case "$frequency" in
         day)    frequency_string="today";;
         week)   frequency_string="the week";;
         month)  frequency_string="the month";;
         year)   frequency_string="the year";;
         all)    frequency_string="all time";;
-        *)   echo "Error: Invalid frequency $FREQUENCY provided. Either leave parameter -f|--frequency empty or set to day|week|month|year|all"; exit 1 ;;
+        *)      echo "Error: Invalid frequency $FREQUENCY provided. Either leave parameter -f|--frequency empty or set to day|week|month|year|all";
+                exit 1 ;;
     esac
     echo "$frequency_string"
 }
@@ -140,18 +183,18 @@ check_http_code() {
         echo "Invalid subreddit /r/$SUBREDDIT, please check the subreddit parameter"
         exit 1
     elif [ "$http_code" -eq "200" ]; then
-        echo "Successfully parsed metadata from /r/$SUBREDDIT"
+        echo "Successfully parsed metadata"
     fi
 }
 
 
 download_images(){
-    dl_dir=$1
+    directory=$1
     frequency=$2
     subreddit=$3
     frequency_string=$(get_frequency_string "$frequency")
 
-    debug_print "DIRECTORY: $dl_dir, frequency: $frequency, subreddit: $subreddit, VERBOSE: $VERBOSE"
+    debug_print "DIRECTORY: $directory, frequency: $frequency, subreddit: $subreddit, VERBOSE: $VERBOSE"
 
     top_url="https://www.reddit.com/r/$subreddit/top.json?limit=100&t=$frequency"
 
@@ -163,12 +206,12 @@ download_images(){
     number_images=$(jq -r '[.data.children | .[].data | select(.post_hint=="image")] | length' posts.json)
     urls=$(jq -r '.data.children | .[].data | select(.post_hint=="image") | .url' posts.json)
 
-    if [ ! -d "$dl_dir" ]; then
-        debug_print "$dl_dir does not exist, creating"
-        mkdir "$dl_dir"
+    if [ ! -d "$directory" ]; then
+        debug_print "$directory does not exist, creating"
+        mkdir "$directory"
     fi
 
-    dl_folder_abs=$(realpath "$dl_dir")
+    dl_folder_abs=$(realpath "$directory")
 
     echo "Downloading top $number_images images of $frequency_string from /r/$subreddit"
 
@@ -202,7 +245,7 @@ download_images(){
 
     echo ""
     echo ""
-    echo "Finished. Total: $total,  downloaded: $downloaded, skipped: $skipped, errors: $errors"
+    echo "Finished. Total: $number_images,  downloaded: $downloaded, skipped: $skipped, errors: $errors"
     if [ -n "$error_urls" ]; then
         echo "Following images could not be downloaded"
         echo "$error_urls"
@@ -212,6 +255,18 @@ download_images(){
 }
 
 parse_args "$@"
-process_args
 
-download_images "$DL_DIR" "$FREQUENCY" "$SUBREDDIT"
+if [ -z "$JSON_FILE" ]; then
+    check_args
+    download_images "$DL_DIR" "$FREQUENCY" "$SUBREDDIT"
+else
+    check_json
+    number_entries=$(jq ". | length" "$JSON_FILE")
+    for entry in $(seq 0 $((number_entries-1))); do
+        subreddit=$(jq -r ".[$entry].subreddit" "$JSON_FILE")
+        frequency=$(jq -r ".[$entry].frequency" "$JSON_FILE")
+        directory=$(eval echo $(jq -r ".[$entry].directory" "$JSON_FILE"))
+        download_images "$directory" "$frequency" "$subreddit"
+    done
+fi
+
